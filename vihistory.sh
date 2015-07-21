d024cd09e7adf58d25ff6f63a604084a161e66fb
ParkJaekwon <insainty@MacBookPro.local>
2015-07-21 10:42:18 +0900

    add to readonly alias

--- vihistory.sh
+++ vihistory.sh
@@ -63,6 +63,7 @@ then
 	if [ -z $(grep "export PROMPT_COMMAND=" /etc/$bashrc) ]
 	then
 		echo "export PROMPT_COMMAND='RETRN_VAL=\$?;logger -p local6.debug \"\$(whoami) [\$\$] [\$PWD]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\"'" >> /etc/$bashrc
+		echo "readonly PROMPT_COMMAND" >> /etc/$bashrc
 	fi
 
 	if [ -z $(grep /var/log/bash_history.log /etc/$log_config) ]
