7c8bd854123c99de3c03e6ba638315fb85c2d648
ParkJaekwon <insainty@MacBookPro.local>
2015-07-28 12:10:56 +0900

    add to client ip logging

--- vihistory.sh
+++ vihistory.sh
@@ -62,7 +62,7 @@ then
 
 	if [ -z $(grep "export PROMPT_COMMAND=" /etc/$bashrc) ]
 	then
-		echo "export PROMPT_COMMAND='RETRN_VAL=\$?;logger -p local6.debug \"\$(whoami) [\$\$] [\$PWD]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\"'" >> /etc/$bashrc
+		echo "export PROMPT_COMMAND='RETRN_VAL=\$?;logger -p local6.debug \"\$(whoami) \$(who am i | awk -F\( '{print $2}' | sed "s/[()]//g") [\$\$] [\$PWD]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" ) [\$RETRN_VAL]\"'" >> /etc/$bashrc
 		echo "readonly PROMPT_COMMAND" >> /etc/$bashrc
 	fi
 
