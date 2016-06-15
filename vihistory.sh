7f8b8fae99d376e2106982b3c91dc7da11fc57fc
jaekwon-park <insainty21@gmail.com>
2016-06-15 16:40:25 +0900

    Update vihistory.sh

--- vihistory.sh
+++ vihistory.sh
@@ -22,7 +22,7 @@ then
 	else
 		#logger -p local6.debug "$(whoami) [$PWD]: Edit started $1"
 		var1=$(mktemp) # make temporay file
-		edited_file=$(echo $(echo $PWD | sed s:\/:_:g)-$(echo $1 | sed s:\/:_:g)-$(openssl rand -hex 2))
+		edited_file=$(echo $(echo $1 | awk -F\/ '{print $NF}')-$(openssl rand -hex 2))
 		cp -p $1 $var1 # copy original file
 		$vim $1
 		temp_IFS=$IFS
